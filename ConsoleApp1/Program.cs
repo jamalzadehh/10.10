6d8eccb4d5f635ca2ff890db54c9908e95de0b3c
Your Name <you@example.com>
2023-10-10 05:39:33 +0400

    file added

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "        Salam,    AzerbAycAnA  XoS     geLmisIniz.   ";
+            string output = FormatSentence(input);
+            Console.WriteLine(output);
+
+        }
+    }
+
+    class Program    //Neyi sehv etdiyimi tapa bilmedim. 
+    {
+        static string FormatSentence(string sentence) { 
+            sentence = sentence.Trim();
+            sentence = string.Join(" ", sentence.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+            TextInfo textInfo = new CultureInfo("a-Z", false).TextInfo;
+            sentence = textInfo.ToTitleCase(sentence.ToLower());
+            return sentence;            
+      }      }
+
+    
+      
+        
+    
+}
